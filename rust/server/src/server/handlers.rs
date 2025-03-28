2708bdf197625760d48be2f60ee0881737841ce6
Alex Su <alexanderytsu@gmail.com>
2025-03-28 13:31:17 +1100

    fix handler error, test ci caching

--- rust/server/src/server/handlers.rs
+++ rust/server/src/server/handlers.rs
@@ -21,7 +21,7 @@ use super::{db, jwt, AppState};
 type ReqContext = AdlReqContext<AppState>;
 
 pub async fn healthy(_ctx: ReqContext, _i: ()) -> HandlerResult<()> {
-    return Ok(());
+    Ok(())
 }
 
 pub async fn login(ctx: ReqContext, i: LoginReq) -> HandlerResult<LoginResp> {
