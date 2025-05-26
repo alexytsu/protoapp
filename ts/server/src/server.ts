85cd556b060ca14857cd084a99e4849c8618a864
Alex Su <alexanderytsu@gmail.com>
2025-05-26 10:07:08 +1000

    fix eslint config for server

--- ts/server/src/server.ts
+++ ts/server/src/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
 
 async function main() {
+  console.log("starting server...");
   app.listen(8080);
+  console.log("listening on port 8080...");
 
   await new Promise<void>(() => {
     // never
